695cb6abe23dba94e6aab4e4153f209f9eb795ba
Nazmul Idris <nazmul@fasterlap.com>
2021-12-02 16:58:01 -0800

    Rewrite UI to be smoother and nicer
    - Use https://github.com/egoist/native-toast to make toasts.
    - Reuse DOM elements to prevent jank when elements are removed and added.

--- index.js
+++ index.js
@@ -18,11 +18,11 @@
 // Main entry point.
 //
 
-/** Kick off the extension's main function every time the page loads. */
+/** Kick off the extension's index function every time the page loads. */
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    shortenCurrentBrowserTabUrl()
+    main()
   },
   false
 )
@@ -32,35 +32,55 @@ document.addEventListener(
 //
 
 /**
- * Can't use `window.location.href` since this is the URL of the popup.html.
+ * Can't use `window.location.href` since this is the URL of the index.html.
  * Instead, get the current tab, and then get its URL.
  */
-const shortenCurrentBrowserTabUrl = () => {
+const main = () => {
   // Get the currently selected tab.
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const current = tabs[0]
     const longUrl = current.url
-    updateUI("Shortening URL ...")
     makeNetworkRequest(longUrl)
   })
 }
 
+const Delays = {
+  preparing: 1500,
+  done: 2500,
+  autoClose: 2500,
+}
+
+const Messages = {
+  // Emoji: https://www.w3schools.com/html/html_emojis.asp
+  preparingHeading: "<span>&#128640;</span> Preparing your short link ...",
+  preparingToast: "Shortening with tinyurl ...",
+}
+
 /**
- * Make a fetch call to tinyurl.com to shorten it. When the response comes in
- * update the UI and auto close the window.
- * More info: https://stackoverflow.com/questions/44123426/how-to-bypass-cors-for-chrome-extension
- * More info: https://dev.to/johnpaulada/synchronous-fetch-with-asyncawait
+ * Make a fetch call to tinyurl.com to shorten it. When the response comes in update the UI and
+ * auto close the window.
+ *
+ * CORS bypass: https://stackoverflow.com/questions/44123426/how-to-bypass-cors-for-chrome-extension
+ * Async fetch: https://dev.to/johnpaulada/synchronous-fetch-with-asyncawait
  */
 const makeNetworkRequest = (longUrl) => {
+  const [linkEl, _] = updateUIWithShortLink(Messages.preparingHeading)
+  linkEl.innerHTML = Messages.preparingHeading.replaceAll(" ", "&nbsp;")
+  showToast(Messages.preparingToast, Delays.preparing, "info")
+
   const _makeAsyncRequest = async () => {
     const tinyWebServiceUrl = `https://tinyurl.com/api-create.php?url=${longUrl}`
     const response = await fetch(tinyWebServiceUrl)
     const shortUrlText = await response.text()
     updateUIWithShortLink(shortUrlText)
+    showToast("URL copied to clipboard!", Delays.done, "success")
     copyToClipboard(shortUrlText)
     triggerAutoCloseWindowWithDelay()
   }
-  _makeAsyncRequest()
+
+  setTimeout(async () => {
+    await _makeAsyncRequest()
+  }, Delays.preparing)
 }
 
 /**
@@ -73,36 +93,53 @@ const createLinkElement = (text) => {
   const linkElement = document.createElement("a")
   const linkText = document.createTextNode(text)
   linkElement.appendChild(linkText)
-  linkElement.title = "URL copied to clipboard, click to close popup"
+  linkElement.title = "Click to close popup"
   linkElement.addEventListener("click", () => {
     window.close()
   })
   return linkElement
 }
 
-/** Replace the H1 element w/ a new H1 element that contains a link */
+/**
+ * Create a reusable heading element that can accept a new link element, everytime this
+ * function is called.
+ * @returns [HTMLLinkElement, HTMLHeadingElement]
+ */
 const updateUIWithShortLink = (text) => {
-  // Create a H1 element.
-  removeElement(document.querySelector("h1"))
-  const element = document.createElement("h1")
-  element.appendChild(createLinkElement(text))
-  document.body.appendChild(element)
+  let headingEl = document.querySelector("h1")
+  if (!headingEl) {
+    // Create a heading element.
+    headingEl = document.createElement("h1")
+    document.body.appendChild(headingEl)
+  }
 
-  // Show toast.
-  showToast("URL copied to clipboard!")
+  // Create a new link element.
+  let linkEl = createLinkElement(text)
+
+  // Clean up heading element.
+  if (headingEl.hasChildNodes()) removeAllChildNodes(headingEl)
+
+  // Add the new link element to the heading element.
+  headingEl.appendChild(linkEl)
+
+  return [linkEl, headingEl]
 }
 
-/**
- * More info: https://github.com/mlcheng/js-toast
- * @param text
- */
-const showToast = (text) => {
-  const options = {
-    settings: {
-      duration: AUTO_WINDOW_CLOSE_TIMEOUT_MS / 2,
-    },
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
   }
-  iqwerty.toast.Toast("URL copied to clipboard!", options)
+}
+
+const showToast = (text, delay, type) => {
+  nativeToast({
+    message: text,
+    position: "north",
+    timeout: delay /* Self destruct in 5 sec. */,
+    type: type,
+    rounded: true,
+    closeOnClick: true,
+  })
 }
 
 /**
@@ -132,18 +169,6 @@ const copyToClipboard = (shortUrl) => {
     )
 }
 
-/** Update the h1 element of the HTML w/ the given text */
-const updateUI = (text) => {
-  // Remove the existing h1 element.
-  removeElement(document.querySelector("h1"))
-
-  // Add a new h1 element to DOM.
-  const element = document.createElement("h1")
-  element.innerText = text
-  document.body.appendChild(element)
-  console.log(text)
-}
-
 /** Helper function to remove the given element (if it exists). */
 const removeElement = (element) => {
   if (element) {
@@ -152,10 +177,8 @@ const removeElement = (element) => {
 }
 
 /** Set a timeout to close the window after short delay. */
-const AUTO_WINDOW_CLOSE_TIMEOUT_MS = 5000
 const triggerAutoCloseWindowWithDelay = () => {
   setTimeout(() => {
-    updateUI("Done!")
     window.close()
-  }, AUTO_WINDOW_CLOSE_TIMEOUT_MS)
+  }, Delays.autoClose)
 }
